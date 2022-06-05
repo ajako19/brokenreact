9e61ca44dcb91ff7889622cdd21dfed5e36c7058
Jakob Pereira <jakepereira2004@gmail.com>
2022-06-05 07:33:28 -0400

    secondpush

--- src/services/getRecipes.ts
+++ src/services/getRecipes.ts
@@ -4,6 +4,6 @@ import Recipe, { Results } from "../models/recipes-model";
 export default function getRecipes():Promise<Recipe[]> {
     return axios.get<Results>(`https://api.spoonacular.com/recipes/complexSearch?apiKey=9ee1581e34964d5d921ea6cc4fe71b37`)
     .then((response) => {
-        response;
+        response.data;
     })
 }
